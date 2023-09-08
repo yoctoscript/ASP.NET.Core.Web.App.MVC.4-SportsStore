c2a113d5777007b968120af15071e45326be8bbb
Yoctoscript <yoctoscript@gmail.com>
2023-09-07 21:45:41 -0700

    Add Identity and admin access

--- SportsSln/SportsStore/Program.cs
+++ SportsSln/SportsStore/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SportsStore.Models;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
@@ -15,9 +16,27 @@ builder.Services.AddSession();
 builder.Services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlServer(
+    builder.Configuration["ConnectionStrings:IdentityConnection"]));
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>();
 var app = builder.Build();
+
+if(app.Environment.IsProduction())
+{
+    app.UseExceptionHandler("/error");
+}
+app.UseRequestLocalization(options =>
+{
+    options.AddSupportedCultures("en-US")
+    .AddSupportedUICultures("en-US")
+    .SetDefaultCulture("en-US");
+});
 app.UseStaticFiles();
 app.UseSession();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllerRoute("categoryPage", "{category}/Page{productPage}", new {Controller="Home", action="Index"});
 app.MapControllerRoute("page", "Page{productPage}", new {Controller="Home", action="Index"});
 app.MapControllerRoute("category", "{category}", new {Controller="Home", action="Index"});
@@ -30,4 +49,5 @@ app.MapFallbackToPage("/admin/{*catchall}", "/Admin/Index");
 
 
 SeedData.EnsurePopulated(app);
+IdentitySeedData.EnsurePopulated(app);
 app.Run();
